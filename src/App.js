5863d7566986d4e558c06a3a1a7f0b237e92851f
abdullahmug2332 <abdullahmug2332@gmail.com>
2023-10-26 23:33:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><div className='container'>
+      <h1>BMI Calculator</h1>
+      <div className="input1">
+        <p>Your Weight :</p>
+        <input type="text" id='weight' placeholder='Entre Your Weight in KGs' />
+      </div>
+      <div className="input2">
+        <p>Your Height :</p>
+        <input type="text" id='height' placeholder='Entre Your Height in Foots' />
+      </div>
+      <button onClick={() => {
+
+        const weight = document.getElementById('weight').value;
+        const height = document.getElementById('height').value;
+        document.getElementById('demo').innerHTML =
+         `${(weight / (height*30.48 )/ (height*30.48 )) * 10000}`
+      }}>Check</button>
+      <h3>Your BMI</h3>
+      <h2 id='demo'></h2>
+    </div>
+    <img src="bmi.png" alt="" />
+    </>
+  )
+}
+
+export default App;
